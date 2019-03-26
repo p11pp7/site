5e92f1199754eed8a5c8b6caae93f67121c2e707
p11pp7 <48938634+p11pp7@users.noreply.github.com>
2019-03-26 09:57:35 +0500

    Add files via upload

---
+++ недошар.php
@@ -0,0 +1,47 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>недошар</title>
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+<style type="text/css">
+body p {
+	font-family: asinastra, "American Captain Cyrillic", BIPs;
+}
+ъеъ {
+	font-family: asinastra, "American Captain Cyrillic", BIPs;
+}
+</style>
+</head>
+
+<body bgcolor="#0066FF">
+<h1 style="font-family: asinastra, 'American Captain Cyrillic', BIPs">Шарарам-2(нет)</h1>
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="550" height="400" id="FlashID" title="недошар">
+  <param name="movie" value="../../../Downloads/pirat_shararam.swf" />
+  <param name="quality" value="high" />
+  <param name="wmode" value="opaque" />
+  <param name="swfversion" value="8.0.35.0" />
+  <!-- Этот тег param предлагает пользователям Flash Player 6.0 r65 и более поздних версий загрузить последнюю версию Flash Player. Удалите его, если не хотите, чтобы пользователи видели запрос. -->
+  <param name="expressinstall" value="Scripts/expressInstall.swf" />
+  <!-- Следующий тег object не поддерживается браузером Internet Explorer. Поэтому скройте его от Internet Explorer при помощи IECC. -->
+  <!--[if !IE]>-->
+  <object type="application/x-shockwave-flash" data="../../../Downloads/pirat_shararam.swf" width="550" height="400">
+    <!--<![endif]-->
+    <param name="quality" value="high" />
+    <param name="wmode" value="opaque" />
+    <param name="swfversion" value="8.0.35.0" />
+    <param name="expressinstall" value="Scripts/expressInstall.swf" />
+    <!-- Браузер отображает следующее альтернативное содержимое для пользователей Flash Player 6.0 и более старых версий. -->
+    <div>
+      <h4>Для содержимого этой страницы требуется более новая версия Adobe Flash Player.</h4>
+      <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Получить проигрыватель Adobe Flash Player" width="112" height="33" /></a></p>
+    </div>
+    <!--[if !IE]>-->
+  </object>
+  <!--<![endif]-->
+</object>
+<script type="text/javascript">
+swfobject.registerObject("FlashID");
+</script>
+</body>
+</html>
